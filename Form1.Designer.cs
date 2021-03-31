6612bd72bed1b81ecd96c46e7dfde424fc9eeb67
MrOlegus <Pleskach.Oleg18@yandex.ru>
2021-04-01 00:37:13 +0300

    Release. RSA.

---
+++ Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿
+namespace RSA_GUI
+{
+    partial class FormRSA
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelP = new System.Windows.Forms.Label();
+            this.textP = new System.Windows.Forms.TextBox();
+            this.textQ = new System.Windows.Forms.TextBox();
+            this.labelQ = new System.Windows.Forms.Label();
+            this.textR = new System.Windows.Forms.TextBox();
+            this.labelR = new System.Windows.Forms.Label();
+            this.textF = new System.Windows.Forms.TextBox();
+            this.labelF = new System.Windows.Forms.Label();
+            this.textE = new System.Windows.Forms.TextBox();
+            this.labelE = new System.Windows.Forms.Label();
+            this.textD = new System.Windows.Forms.TextBox();
+            this.labelD = new System.Windows.Forms.Label();
+            this.textOrigin = new System.Windows.Forms.TextBox();
+            this.labelCaption = new System.Windows.Forms.Label();
+            this.textCrypt = new System.Windows.Forms.TextBox();
+            this.buttonCrypt = new System.Windows.Forms.Button();
+            this.buttonDecrypt = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelP
+            // 
+            this.labelP.AutoSize = true;
+            this.labelP.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelP.Location = new System.Drawing.Point(12, 72);
+            this.labelP.Name = "labelP";
+            this.labelP.Size = new System.Drawing.Size(43, 48);
+            this.labelP.TabIndex = 0;
+            this.labelP.Text = "p";
+            // 
+            // textP
+            // 
+            this.textP.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textP.Location = new System.Drawing.Point(61, 72);
+            this.textP.Name = "textP";
+            this.textP.Size = new System.Drawing.Size(163, 55);
+            this.textP.TabIndex = 1;
+            this.textP.TextChanged += new System.EventHandler(this.textD_TextChanged);
+            // 
+            // textQ
+            // 
+            this.textQ.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textQ.Location = new System.Drawing.Point(61, 133);
+            this.textQ.Name = "textQ";
+            this.textQ.Size = new System.Drawing.Size(163, 55);
+            this.textQ.TabIndex = 3;
+            this.textQ.TextChanged += new System.EventHandler(this.textD_TextChanged);
+            // 
+            // labelQ
+            // 
+            this.labelQ.AutoSize = true;
+            this.labelQ.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelQ.Location = new System.Drawing.Point(12, 133);
+            this.labelQ.Name = "labelQ";
+            this.labelQ.Size = new System.Drawing.Size(43, 48);
+            this.labelQ.TabIndex = 2;
+            this.labelQ.Text = "q";
+            // 
+            // textR
+            // 
+            this.textR.Enabled = false;
+            this.textR.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textR.Location = new System.Drawing.Point(61, 194);
+            this.textR.Name = "textR";
+            this.textR.Size = new System.Drawing.Size(163, 55);
+            this.textR.TabIndex = 5;
+            this.textR.TextChanged += new System.EventHandler(this.textD_TextChanged);
+            // 
+            // labelR
+            // 
+            this.labelR.AutoSize = true;
+            this.labelR.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelR.Location = new System.Drawing.Point(12, 194);
+            this.labelR.Name = "labelR";
+            this.labelR.Size = new System.Drawing.Size(34, 48);
+            this.labelR.TabIndex = 4;
+            this.labelR.Text = "r";
+            // 
+            // textF
+            // 
+            this.textF.Enabled = false;
+            this.textF.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textF.Location = new System.Drawing.Point(61, 255);
+            this.textF.Name = "textF";
+            this.textF.Size = new System.Drawing.Size(163, 55);
+            this.textF.TabIndex = 7;
+            this.textF.TextChanged += new System.EventHandler(this.textD_TextChanged);
+            // 
+            // labelF
+            // 
+            this.labelF.AutoSize = true;
+            this.labelF.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelF.Location = new System.Drawing.Point(12, 255);
+            this.labelF.Name = "labelF";
+            this.labelF.Size = new System.Drawing.Size(32, 48);
+            this.labelF.TabIndex = 6;
+            this.labelF.Text = "f";
+            // 
+            // textE
+            // 
+            this.textE.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textE.Location = new System.Drawing.Point(61, 316);
+            this.textE.Name = "textE";
+            this.textE.Size = new System.Drawing.Size(163, 55);
+            this.textE.TabIndex = 9;
+            this.textE.TextChanged += new System.EventHandler(this.textD_TextChanged);
+            // 
+            // labelE
+            // 
+            this.labelE.AutoSize = true;
+            this.labelE.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelE.Location = new System.Drawing.Point(12, 316);
+            this.labelE.Name = "labelE";
+            this.labelE.Size = new System.Drawing.Size(43, 48);
+            this.labelE.TabIndex = 8;
+            this.labelE.Text = "e";
+            // 
+            // textD
+            // 
+            this.textD.Enabled = false;
+            this.textD.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textD.Location = new System.Drawing.Point(61, 377);
+            this.textD.Name = "textD";
+            this.textD.Size = new System.Drawing.Size(163, 55);
+            this.textD.TabIndex = 11;
+            this.textD.TextChanged += new System.EventHandler(this.textD_TextChanged);
+            // 
+            // labelD
+            // 
+            this.labelD.AutoSize = true;
+            this.labelD.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelD.Location = new System.Drawing.Point(12, 377);
+            this.labelD.Name = "labelD";
+            this.labelD.Size = new System.Drawing.Size(43, 48);
+            this.labelD.TabIndex = 10;
+            this.labelD.Text = "d";
+            // 
+            // textOrigin
+            // 
+            this.textOrigin.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textOrigin.Location = new System.Drawing.Point(230, 72);
+            this.textOrigin.Multiline = true;
+            this.textOrigin.Name = "textOrigin";
+            this.textOrigin.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textOrigin.Size = new System.Drawing.Size(605, 177);
+            this.textOrigin.TabIndex = 12;
+            // 
+            // labelCaption
+            // 
+            this.labelCaption.AutoSize = true;
+            this.labelCaption.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelCaption.Location = new System.Drawing.Point(102, 9);
+            this.labelCaption.Name = "labelCaption";
+            this.labelCaption.Size = new System.Drawing.Size(617, 48);
+            this.labelCaption.TabIndex = 13;
+            this.labelCaption.Text = "Шифратор и дешифратор RSA";
+            // 
+            // textCrypt
+            // 
+            this.textCrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textCrypt.Location = new System.Drawing.Point(230, 255);
+            this.textCrypt.Multiline = true;
+            this.textCrypt.Name = "textCrypt";
+            this.textCrypt.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textCrypt.Size = new System.Drawing.Size(605, 177);
+            this.textCrypt.TabIndex = 14;
+            // 
+            // buttonCrypt
+            // 
+            this.buttonCrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonCrypt.Location = new System.Drawing.Point(229, 438);
+            this.buttonCrypt.Name = "buttonCrypt";
+            this.buttonCrypt.Size = new System.Drawing.Size(300, 52);
+            this.buttonCrypt.TabIndex = 15;
+            this.buttonCrypt.Text = "Зашифровать";
+            this.buttonCrypt.UseVisualStyleBackColor = true;
+            this.buttonCrypt.Click += new System.EventHandler(this.buttonCrypt_Click);
+            // 
+            // buttonDecrypt
+            // 
+            this.buttonDecrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonDecrypt.Location = new System.Drawing.Point(535, 438);
+            this.buttonDecrypt.Name = "buttonDecrypt";
+            this.buttonDecrypt.Size = new System.Drawing.Size(300, 52);
+            this.buttonDecrypt.TabIndex = 16;
+            this.buttonDecrypt.Text = "Расшифровать";
+            this.buttonDecrypt.UseVisualStyleBackColor = true;
+            this.buttonDecrypt.Click += new System.EventHandler(this.buttonDecrypt_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(61, 438);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 52);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Случайно";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FormRSA
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SeaGreen;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(845, 498);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonDecrypt);
+            this.Controls.Add(this.buttonCrypt);
+            this.Controls.Add(this.textCrypt);
+            this.Controls.Add(this.labelCaption);
+            this.Controls.Add(this.textOrigin);
+            this.Controls.Add(this.textD);
+            this.Controls.Add(this.labelD);
+            this.Controls.Add(this.textE);
+            this.Controls.Add(this.labelE);
+            this.Controls.Add(this.textF);
+            this.Controls.Add(this.labelF);
+            this.Controls.Add(this.textR);
+            this.Controls.Add(this.labelR);
+            this.Controls.Add(this.textQ);
+            this.Controls.Add(this.labelQ);
+            this.Controls.Add(this.textP);
+            this.Controls.Add(this.labelP);
+            this.Name = "FormRSA";
+            this.Text = "RSA";
+            this.Load += new System.EventHandler(this.FormRSA_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelP;
+        private System.Windows.Forms.TextBox textP;
+        private System.Windows.Forms.TextBox textQ;
+        private System.Windows.Forms.Label labelQ;
+        private System.Windows.Forms.TextBox textR;
+        private System.Windows.Forms.Label labelR;
+        private System.Windows.Forms.TextBox textF;
+        private System.Windows.Forms.Label labelF;
+        private System.Windows.Forms.TextBox textE;
+        private System.Windows.Forms.Label labelE;
+        private System.Windows.Forms.TextBox textD;
+        private System.Windows.Forms.Label labelD;
+        private System.Windows.Forms.TextBox textOrigin;
+        private System.Windows.Forms.Label labelCaption;
+        private System.Windows.Forms.TextBox textCrypt;
+        private System.Windows.Forms.Button buttonCrypt;
+        private System.Windows.Forms.Button buttonDecrypt;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RSA_GUI
+{
+    public partial class FormRSA : Form
+    {
+		const ulong maxNumber = 1000;
+
+		List<ulong> prime = new List<ulong>();
+
+		ulong p, q, r, f, E, d;
+
+		string message;                                 // искомый текст
+		List<ulong> crypt = new List<ulong>();          // зашифрованный текст
+
+		void findPrimeNumbers()
+		{
+			for (ulong i = 2; i <= maxNumber; i++)
+			{
+				bool isPrime = true;
+				for (short j = 0; j < prime.Count; j++)
+					if (i % prime[j] == 0)
+					{
+						isPrime = false;
+						break;
+					}
+				if (isPrime)
+				{
+					prime.Add(i);
+				}
+			}
+		}
+
+		ulong NOD(ulong a, ulong b)
+		{
+			while (a > 0 && b > 0)
+				if (a > b) a %= b;
+				else b %= a;
+			return a + b;
+		}
+
+		List<ulong> getPrimaryMas(ulong n)
+		{
+			List<ulong> primaryMas = new List<ulong>();
+			for (ulong i = 1; i <= maxNumber; i++)
+				if (NOD(i, n) == 1)
+				{
+					primaryMas.Add(i);
+				}
+			return primaryMas;
+		}
+
+		Random rnd = new Random();                    // объект для получения случайных чисел
+
+		ulong Euclidex(ulong a, ulong b)
+		{
+			long d0 = (long)a; long d1 = (long)b;
+			long x0 = 1; long x1 = 0;
+			long y0 = 0; long y1 = 1;
+			while (d1 > 1)
+			{
+				long q = d0 / d1;
+				long d2 = d0 % d1;
+				long x2 = x0 - q * x1;
+				long y2 = y0 - q * y1;
+				d0 = d1; d1 = d2;
+				x0 = x1; x1 = x2;
+				y0 = y1; y1 = y2;
+			}
+			if (y1 < 0) y1 += (long)a;
+			return (ulong)y1;
+		}
+
+        ulong fastExp(ulong a, ulong z, ulong n)
+		{
+			ulong a1 = a;
+			ulong z1 = z;
+			ulong x = 1;
+			while (z1 != 0)
+			{
+				while ((z1 % 2) == 0)
+				{
+					z1 = z1 / 2;
+					a1 = (a1 * a1) % n;
+				}
+				z1 -= 1;
+				x = (x * a1) % n;
+			}
+			return x;
+		}
+
+		ulong getNumber(char letter)
+		{
+			if ((letter >= 'a') && (letter <= 'z')) return (ulong)letter - 'a' + 2;
+			if ((letter >= 'A') && (letter <= 'Z')) return (ulong)letter - 'A' + 2;
+			if (letter == ' ') return 28; // пробел считаем символом алфавита
+			return 0;
+		}
+
+		char getLetter(ulong number)
+        {
+			if ((number >= 2) && (number <= 27)) return (char)('a' + number - 2);
+			if (number == 28) return ' ';
+			return '\0';
+        }
+
+        private void textD_TextChanged(object sender, EventArgs e) // изменение параметров
+        {
+			try
+			{
+				switch (((TextBox)sender).Name)
+				{
+					case "textP":
+						p = (ulong)Convert.ToInt32(textP.Text); break;
+					case "textQ":
+						q = (ulong)Convert.ToInt32(textQ.Text); break;
+					case "textE":
+						E = (ulong)Convert.ToInt32(textE.Text); break;
+				}
+				r = p * q;
+				f = (p - 1) * (q - 1);
+				d = Euclidex(f, E);
+				textR.Text = Convert.ToString(r);
+				textF.Text = Convert.ToString(f);
+				textD.Text = Convert.ToString(d);
+			}
+			catch
+            {
+
+            }
+		}
+
+        private void FormRSA_Load(object sender, EventArgs e)
+        {
+			findPrimeNumbers();                            // получения массива простых чисел
+			MessageBox.Show("Параметры можно водить самостоятельно, а можно сгенерировать их случайно. Если параметры введены неверно, программа уведомит Вас об этом.\r\nЯзык - английский.\r\nШифруются английские буквы и пробелы, остальное - удаляется.");
+		}
+
+        bool isValid() // проверка на корректность параметров
+        {
+			bool isPPrime = false;
+			bool isQPrime = false;
+
+			for (int i = 0; i < prime.Count; i++)
+            {
+				if (p == prime[i]) isPPrime = true;
+				if (q == prime[i]) isQPrime = true;
+			}
+
+			if (!isPPrime) 
+			{
+				MessageBox.Show("Число p - не простое!");
+				return false;
+			}
+			if (!isQPrime)
+			{
+				MessageBox.Show("Число q - не простое");
+				return false;
+			}
+			if (p == q)
+            {
+				MessageBox.Show("Параметры p и q не должны быть равны");
+				return false;
+			}
+			if (r != p * q)
+			{
+				MessageBox.Show("Неверный параметр r");
+				return false;
+			}
+			if (r < 30)
+			{
+				MessageBox.Show("Параметр r не должен быть меньше 30");
+				return false;
+			}
+			if (f != (p - 1) * (q - 1))
+			{
+				MessageBox.Show("Неверный параметр f");
+				return false;
+			}
+			if (NOD(E, f) != 1)
+			{
+				MessageBox.Show("e и f - не взаимнопросты");
+				return false;
+			}
+			if ((E * d) % f != 1)
+			{
+				MessageBox.Show("Неверный параметр d");
+				return false;
+			}
+			return true;
+        }
+
+		public FormRSA()
+        {
+            InitializeComponent();
+        }
+
+		private void buttonCrypt_Click(object sender, EventArgs e)
+		{
+			if (!isValid())
+			{
+				return;
+			}
+
+			message = textOrigin.Text;           // искомый текст
+			for (int i = 0; i < message.Length; i++)
+			{
+				if (((message[i] < 'a') || (message[i] > 'z')) &&
+					((message[i] < 'A') || (message[i] > 'Z')) &&
+					(message[i] != ' '))
+                {
+					message = message.Remove(i, 1);
+					i--;
+                }
+            }
+			textOrigin.Text = message;
+
+		    crypt = new List<ulong>();          // зашифрованный текст
+
+			for (int i = 0; i < message.Length; i++)
+			{
+				crypt.Add(fastExp(getNumber(message[i]), E, r));
+			}
+
+			textCrypt.Text = "";
+			for (ushort i = 0; i < crypt.Count; i++)
+			{
+				textCrypt.Text += crypt[i].ToString();
+				textCrypt.Text += " ";
+			}
+		}
+
+		private void buttonDecrypt_Click(object sender, EventArgs e)
+		{
+			message = "";
+			for (int i = 0; i < crypt.Count; i++)
+            {
+				message += getLetter(fastExp(crypt[i], d, r));
+            }
+			textOrigin.Text = message;
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+        {
+			p = prime[rnd.Next(0, prime.Count)];      // получение параметра p
+			q = prime[rnd.Next(0, prime.Count)];        // получение параметра q
+			r = p * q;                                // получение параметра r
+			f = (p - 1) * (q - 1);                    // получение значения функции Эйлера
+			List<ulong> primaryMas = getPrimaryMas(f);   // получение чисел, взаимнопростых с f
+			E = primaryMas[rnd.Next(0, primaryMas.Count - 1)];           // получение параметра e
+			d = Euclidex(f, E);                       // получение параметра d
+
+			textP.Text = Convert.ToString(p);
+			textQ.Text = Convert.ToString(q);
+			textR.Text = Convert.ToString(r);
+			textF.Text = Convert.ToString(f);
+			textE.Text = Convert.ToString(E);
+			textD.Text = Convert.ToString(d);
+		}
+	}
+}
